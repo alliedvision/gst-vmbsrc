5f8502e3ef1e86e83a12f8f7144f1e5f4d83c19b
Niklas Kröger <Niklas.Kroeger@alliedvision.com>
2021-02-09 15:12:13 +0100

    Adjust license to only allow LGPL 2.0

--- src/gstvimbasrc.c
+++ src/gstvimbasrc.c
@@ -3,8 +3,7 @@
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * License version 2.0 as published by the Free Software Foundation.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,8 +12,8 @@
  *
  * You should have received a copy of the GNU Library General Public
  * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Suite 500,
- * Boston, MA 02110-1335, USA.
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 /**
  * SECTION:element-gstvimbasrc
